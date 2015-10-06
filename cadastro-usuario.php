eb847f6d5ed25a06a6f4d3c6102ba15db0b90a80
AlexFabiano <alex_fabino1993@hotmail.com>
2015-10-06 03:11:41 -0300

    ATULIZADO

--- cadastro-usuario.php
+++ cadastro-usuario.php
@@ -3,11 +3,18 @@
                             include_once 'includes/usuario.php';
 
                             if (isset($_POST['nome']) && $_POST['nome'] != "" 
-                            && isset($_POST['cpf']) && $_POST['cpf'] != "" 
-                             && isset($_POST['idade']) && $_POST['idade'] != "") { 
+                            && isset($_POST['username']) && $_POST['username'] != "" 
+                            && isset($_POST['email']) && $_POST['email'] != ""  
+                            && isset($_POST['cpf']) && $_POST['cpf'] != ""  
+                            && isset($_POST['idade']) && $_POST['idade'] != "" 
+                            && isset($_POST['login']) && $_POST['login'] != ""
+                            && isset($_POST['senha']) && $_POST['senha'] != "" 
+                            && isset($_POST['tipo_usuario']) && $_POST['tipo_usuario'] != "") { 
                            
-
-                                  $retorno = cadastrarUsuario();
+                              
+                                
+                                 
+                                $retorno = cadastrarUsuario();
 
                                 if ($retorno) {
                                 header('Location: cadastro-usuarios-ok.php');
@@ -17,6 +24,10 @@
                                  } else {
                                  echo "Preencha todos os campos!";
                                 }
+                                
+                                
+                                  
+                               
                ?>
     <form action="" method="POST">
                  <br>
@@ -26,16 +37,50 @@
     }
     ?>
     <br>
-   Cpf<input type="text" name="cpf">  
-    <?php if (isset($_POST['cpf']) && $_POST['idade'] == "") {
+   username<input type="text" name="username">  
+    <?php if (isset($_POST['username']) && $_POST['username'] == "") {
         echo "<span style=\"color:red;\">Campo email eh obrigatorio!!</span>";
     }
     ?><br>
    
-    Idade<input type="text" name="idade">  
+    email<input type="text" name="email">  
+    <?php if (isset($_POST['email']) && $_POST['email'] == "") {
+        echo "<span style=\"color:red;\">Campo email eh obrigatorio!!</span>";
+    }
+    ?><br>
+    
+    cpf<input type="text" name="cpf">  
+    <?php if (isset($_POST['cpf']) && $_POST['cpf'] == "") {
+        echo "<span style=\"color:red;\">Campo email eh obrigatorio!!</span>";
+    }
+    ?><br>
+    
+    idade<input type="text" name="idade">  
     <?php if (isset($_POST['idade']) && $_POST['idade'] == "") {
         echo "<span style=\"color:red;\">Campo email eh obrigatorio!!</span>";
     }
+    
+    ?><br>
+    
+    login<input type="text" name="login">  
+    <?php if (isset($_POST['login']) && $_POST['login'] == "") {
+        echo "<span style=\"color:red;\">Campo email eh obrigatorio!!</span>";
+    }
+    
+    ?><br>
+    
+    senha<input type="text" name="senha">  
+    <?php if (isset($_POST['senha']) && $_POST['senha'] == "") {
+        echo "<span style=\"color:red;\">Campo email eh obrigatorio!!</span>";
+    }
+    
+    ?><br>
+   
+    tipo<input type="text" name="tipo_usuario">  
+    <?php if (isset($_POST['tipo_usuario']) && $_POST['tipo_usuario'] == "") {
+        echo "<span style=\"color:red;\">Campo email eh obrigatorio!!</span>";
+    }
+    
     ?><br>
     <input type="submit" value="SALVAR"> 
 </form>
@@ -43,7 +88,16 @@
 
                 
 
-   
+     <!-- exemplo do banco
+                                  $query->bindValue(':nome', $_POST['nome']);
+                                  $query->bindValue(':username', $username);
+                                  $query->bindValue(':email', $_POST['email']);
+                                  $query->bindValue(':cpf', $_POST['cpf']);
+                                  $query->bindValue(':idade', $_POST['idade']);
+                                  $query->bindValue(':login', $_POST['login']);
+                                  $query->bindValue(':senha', $_POST['senha']);
+                                  $query->bindValue(':tipo_usuario', $_POST['tipo_usuario']);    
+                                -->
   
    
           
